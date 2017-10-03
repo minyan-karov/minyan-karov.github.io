99df565e88d686cb7d16b8bcbdf8ed63dbc6785f
minyan-karov <32475981+minyan-karov@users.noreply.github.com>
2017-10-03 18:41:40 +0300

    Add files via upload

--- senagog-map/senagog-map.component.js
+++ senagog-map/senagog-map.component.js
@@ -3,15 +3,17 @@ angular.
   component('senagogMap', {
         templateUrl : "senagog-map/senagog-map.htm",
         controller : function () {
+            
             var uluru = {lat: -25.363, lng: 131.044};
+            
             var map = new google.maps.Map(document.getElementById('map'), {
                 zoom: 4,
                 center: uluru
             });
 
             var marker = new google.maps.Marker({
-            position: uluru,
-            map: map
+                position: uluru,
+                map: map
             });
         }
     });
