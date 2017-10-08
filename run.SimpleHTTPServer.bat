eee7c7e1f2f0f8082a07f2c9da074b8956942d28
minyan-karov <32475981+minyan-karov@users.noreply.github.com>
2017-10-08 09:52:33 +0300

    Add files via upload

--- run.SimpleHTTPServer.bat
+++ run.SimpleHTTPServer.bat
@@ -6,4 +6,4 @@ rem if not working, go to http://192.168.1.two:8000
 
 
 
-rem 8234
+rem for python 3 - "python -m http.server 8234"
--- senagog-map/senagog-map.component.js
+++ senagog-map/senagog-map.component.js
@@ -5,22 +5,19 @@ angular.
         controller : function ($http) {
             
             var self = this;
-
-            var uluru = {lat: -25.363, lng: 131.044};
+            
+            //Tel Aviv - 32.086718, 34.789760
+            var telAviv = {lat: 32.086718, lng: 34.789760};
             
             var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 4,
-                center: uluru
+                zoom: 9,
+                center: telAviv
             });
 
-            var marker = new google.maps.Marker({
-                position: uluru,
-                map: map
-            });
-
-            $http.get('https://script.googleusercontent.com/macros/s/AKfycbxWJgCP0uIg5AgVxORyPWbZw4p7IJotc1aIcIdNFFqjmzIpUog/exec?fun=getSenagogs').then(
+            $http.get("https://script.google.com/macros/s/AKfycbxWJgCP0uIg5AgVxORyPWbZw4p7IJotc1aIcIdNFFqjmzIpUog/exec?fun=getSenagogs").then(
                 function(response) {
                     self.senagogs = response.data;
+                    drowSenagogs(response.data, map)
                 }
             );
 
@@ -28,3 +25,54 @@ angular.
         }
 
     });
+
+    function drowSenagogs(senagogs, map){
+        console.log(senagogs);
+        senagogs.forEach(function (senagog)
+        {
+            var coordinate = senagog.coordinate.split(",");
+            var coordinateForMap = {lat: Number(coordinate[0]), lng: Number(coordinate[1])};
+
+            var marker = new google.maps.Marker({
+                position: coordinateForMap,
+                map: map,
+                title: senagog.senagogName
+            });
+
+
+            var contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">'+ senagog.senagogName +'</h1>'+
+                '<div id="bodyContent">'+
+                '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                'sandstone rock formation in the southern part of the '+
+                'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                'Aboriginal people of the area. It has many springs, waterholes, '+
+                'rock caves and ancient paintings. Uluru is listed as a World '+
+                'Heritage Site.</p>'+
+                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                '(last visited June 22, 2009).</p>'+
+                '</div>'+
+                '</div>';
+
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+
+            marker.addListener('click', function() {
+                infowindow.open(map, marker);
+            });
+
+
+
+
+            console.log(senagog.coordinate.split(","));
+            console.log(coordinateForMap);
+        });
+    }
